676439968fdb5365b40ca6f63669e2e8a38a73cf
bluther28 <b.luther28@hotmail.com>
2017-04-25 22:12:42 -0500

    Updated LRU and Main
    LRU completed and Main Updated

---
+++ lru.py
@@ -0,0 +1,40 @@
+# Least Recently Used
+# This algorithm takes out the least recently used page
+
+def lru(input, frames):
+	pages = [] # Pages stored from input
+	stamps = {} # Time stamps of pages in input.
+	faults = 0 # Number of page faults
+	time = 1
+	
+	# Initialize stamps for each page in input.
+	for page in input:
+		stamps[page] = 0
+
+	for page in input:
+		print("page", page)
+		
+		stamps[page] = time
+
+		print("Times", stamps)
+		
+		if not page in pages:
+			faults += 1
+
+			if len(pages) < frames:
+				pages.append(page)
+			else:
+				least = 0
+				i = 1
+				while i < len(pages):
+					if stamps[pages[i]] < stamps[pages[least]]:
+						least = i
+					i += 1
+
+				pages[least] = page
+
+		print("pages", pages)
+		time+=1
+		
+	return faults
+	
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import optimal
 import fifo
 import lfu
+import lru
 
 input = [1,2,3,4,1,2,5,1,2,3,4,5]
 
 #print(fifo.fifo(input, 5))
 #print(optimal.optimal(input, 3))
-print(lfu.lfu(input, 3))
+#print(lfu.lfu(input, 3))
+print(lru.lru(input,3))
